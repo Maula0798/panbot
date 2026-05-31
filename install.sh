a810df1dc06537adea3ba24bf0f84de1ca2b5a83
keonggaspoll <95425941+Maula0798@users.noreply.github.com>
2026-05-31 10:23:25 +0700

    Update install.sh to read Windows password
    Prompt user for Windows password and use it in script.

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+
+
+read -sp "Masukkan password Windows: " PASS
+
+echo
+
+
+
 wget -q -O reinstall.sh https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh
+
 chmod +x reinstall.sh
 
+
+
 bash reinstall.sh dd \
+
   --rdp-port "3778" \
+
   --ssh-port 2222 \
-  --password "Isengiseng77" \
+
+  --password "$PASS" \
+
   --img "https://pub-bc43ae568e3844ffa3309f40355ac836.r2.dev/windows11.img.gz"
+
+
 
 reboot
