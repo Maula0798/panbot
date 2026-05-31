832f4893e7eb64e28c8029ba3a4299b0121dd94b
keonggaspoll <95425941+Maula0798@users.noreply.github.com>
2026-05-31 10:21:33 +0700

    Add install script to download and run reinstall.sh
    This script downloads and executes a reinstall script with specified parameters for RDP, SSH, and a Windows 11 image.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -q -O reinstall.sh https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh
+chmod +x reinstall.sh
+
+bash reinstall.sh dd \
+  --rdp-port "3778" \
+  --ssh-port 2222 \
+  --password "Isengiseng77" \
+  --img "https://pub-bc43ae568e3844ffa3309f40355ac836.r2.dev/windows11.img.gz"
+
+reboot
